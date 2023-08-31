6bba40ab414545581ac80d4d372efe5e4587723b
Lezette <lezette427@gmail.com>
2023-08-31 02:21:02 -0400

    install react-bootstrap and start About Me page

---
+++ src/sections/MyPassion.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import '../App.css';
+import { useColors } from '../components/ColorProvider.tsx';
+import me from '../images/me.jpg';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+const MyPassion = () => {
+
+  // create multiple color instances so that each h2 will have different colors
+//  const { color1, color2 } = useColors();
+
+
+function Passion() {
+  return(
+    <div>
+      <h5>Recent UCF graduate with a Bachelor's degree in Computer Science with the goal of working in Web Development, specifically front-end using frameworks like ReactJS. My ultimate goal is to learn all aspects of web development and be a full stack developer.
+      <br />
+      <br />
+      When encountering a company, people instinctively turn to their website, effectively serving as the company's virtual facade. This initial encounter becomes their first impression of the organization, making it imperative to craft a website that is not only intuitive but also visually captivating. Personally, I find immense joy in unleashing my creativity through web development, for the possibilities within this realm are limitless. My aspiration is to build websites that provide a lasting and memorable experience for their visitors.</h5>
+    </div>
+  );
+}
+
+function About() {
+  return(
+    <div>
+      <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
+    </div>
+  );
+}
+
+function Education() {
+  return(
+    <div>
+      <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
+    </div>
+  );
+}
+
+function Hobbies() {
+  return(
+    <div>
+      <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</h5>
+    </div>
+  );
+}
+
+  return(
+
+    <div className='columns-2'>
+      <div>
+        <img src={me} height={600}/>
+      </div>
+      <div className='container'>
+        <Tabs
+          defaultActiveKey="my-passion"
+          id="fill-tab-example"
+          className="mb-3 border-0"
+          fill
+        >
+          <Tab eventKey="my-passion" title="My Passion" tabClassName='tab-title'>
+            <Passion />
+          </Tab>
+          <Tab eventKey="about-me" title="About Me" tabClassName='tab-title'>
+            <About />
+          </Tab>
+          <Tab eventKey="education" title="Education" tabClassName='tab-title'>
+            <Education />
+          </Tab>
+          <Tab eventKey="hobbies" title="Hobbies" tabClassName='tab-title'>
+            <Hobbies />
+          </Tab>
+        </Tabs>
+      </div>
+    </div>
+
+  );
+}
+
+export default MyPassion;
--- src/sections/Narrative.tsx
+++ src/sections/Narrative.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import '../App.css';
 import { useColors } from '../components/ColorProvider.tsx';
+import { Link } from 'react-router-dom';
 
 const Narrative = () => {
 
   const { color1, color2 } = useColors();
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Toggle the "yellow" class on the current active line
+      document.querySelector(`.line${activeIndex + 1}`).classList.toggle('active');
+
+      // Move to the next line and reset if it exceeds the total number of lines
+      setActiveIndex((prevIndex) => (prevIndex + 1) % 3);
+    }, 1000); // Change line color every second
+
+    return () => clearInterval(intervalId);
+  }, [activeIndex]);
 
   return(
     <div className="page">
       <div className='title-container container'>
         <h1 className="page-title flex justify-start" style={{ color: color1 }}>NARRATIVE OF ME</h1>
-        <h3 className='flex justify-end leading-6'>"Combining creativity and diligence</h3>
-        <h3 className='flex justify-end mb-5'>to forge extraordinary results."</h3>
-        <p className='flex justify-end mt-10'>Explore further.</p>
+        <div className='text-container'>
+          <h3 className={`flex justify-end leading-6 line line1 ${activeIndex === 0 ? 'active' : ''}`}>"Combining creativity and diligence</h3>
+          <h3 className={`flex justify-end mb-5 line line2 ${activeIndex === 1 ? 'active' : ''}`}>to forge extraordinary results."</h3>
+          <Link to='/my-passion' className={`p_link flex justify-end mt-10 line line3 ${activeIndex === 2 ? 'active' : ''}`}>Explore further.</Link>
+        </div>
       </div>
     </div>
   )
