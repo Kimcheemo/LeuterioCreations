c2bf3d01cba5a6bf6a1ac41e0ed5543b509e07ac
Lezette <lezette427@gmail.com>
2023-08-31 03:23:55 -0400

    revert back to working titles without bootstrap css

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,33 @@ import Showcase from './sections/Showcase.tsx';
 import Contact from './sections/Contact.tsx';
 import { useRef, useState, useEffect } from 'react';
 import { ColorProvider } from './components/ColorProvider.tsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import MyPassion from './sections/MyPassion.tsx';
 
-const sections = [
-  { title: 'LEUTERIO CREATIONS', content: <Title /> },
-  { title: 'CONTACT', content: <Contact /> },
-  { title: 'SHOWCASE', content: <Showcase /> },
-  { title: 'NARRATIVE', content: <Narrative /> }
-];
 
 
 const App: React.FC = () => {
 
   return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout />} />
+        {/*<Route path='/my-passion' element={<MyPassion />} />*/}
+      </Routes>
+    </Router>
+  );
+};
+
+const Layout = () => {
+  const sections = [
+    { title: 'LEUTERIO CREATIONS', content: <Title /> },
+    { title: 'CONTACT', content: <Contact /> },
+    { title: 'SHOWCASE', content: <Showcase /> },
+    { title: 'NARRATIVE', content: <Narrative /> }
+  ];
+
+  return(
     <ColorProvider>
       <div>
         <WheelContainer sections={sections} />
--- src/sections/Showcase.tsx
+++ src/sections/Showcase.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import '../App.css';
 import { useColors } from '../components/ColorProvider.tsx';
 
 const Showcase = () => {
 
   const { color1, color2 } = useColors();
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Toggle the "yellow" class on the current active line
+      document.querySelector(`.line${activeIndex + 1}`).classList.toggle('active');
+
+      // Move to the next line and reset if it exceeds the total number of lines
+      setActiveIndex((prevIndex) => (prevIndex + 1) % 3);
+    }, 1000); // Change line color every second
+
+    return () => clearInterval(intervalId);
+  }, [activeIndex]);
 
   return(
     <div className="page">
       <div className='container-sm'>
-        <h4 className='flex justify-start mb-0'>"Unwavering dedication,</h4>
-        <h4 className='flex justify-end mt-0'>boundless creativity."</h4>
+        <h4 className={`flex justify-start mb-0 line line1 ${activeIndex === 0 ? 'active' : ''}`}>"Unwavering dedication,</h4>
+        <h4 className={`flex justify-end mt-0 line line2 ${activeIndex === 1 ? 'active' : ''}`}>boundless creativity."</h4>
         <h1 className="page-title mb-12" style={{ color: color2 }}>SHOWCASE</h1>
-        <p className='flex justify-end mt-12'>Discover more.</p>
+        <p className={`flex justify-end line line3 ${activeIndex === 2 ? 'active' : ''}`} style={{marginTop: "65px", paddingLeft: "490px"}}>Discover more.</p>
       </div>
     </div>
   )
