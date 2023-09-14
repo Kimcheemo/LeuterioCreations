1554d8ce7f9abbc0652f6a3e07fe9c523a0ff008
Lezette <lezette427@gmail.com>
2023-09-14 16:36:04 -0400

    finish MyPassion

--- src/sections/MyPassion.tsx
+++ src/sections/MyPassion.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useColors } from '../components/ColorProvider.tsx';
 import me from '../images/me.jpg';
 import '../styles/Narrative.css';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Logo from '../components/Logo.tsx';
+import arrow from '../images/back.png';
 
 const MyPassion = () => {
 
   // create multiple color instances so that each h2 will have different colors
 //  const { color1, color2 } = useColors();
+const navigate = useNavigate();
 const [activeTab, setActiveTab] = useState("MY PASSION");
 
 function Passion() {
@@ -31,7 +34,7 @@ function About() {
   );
 }
 
-function Education() {
+function Skills() {
   return(
     <div>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -47,23 +50,39 @@ function Hobbies() {
   );
 }
 
+const tabColors = {
+    "MY PASSION": '#523589',
+    "ABOUT ME": "#CB60B8",
+    "MY SKILLS": "#203972",
+    "MY HOBBIES": "#21BAB1",
+  };
+
+  const getTabColor = () => {
+    return tabColors[activeTab] || "white"; // Default to black if no color is found
+  };
+
+  // goes back to main page
+  const handleNavigation = () => {
+    navigate(-1);
+  };
+
   return(
     <div className='flex mx-2'>
 
-      <div className='mx-5 my-24'>
+      <div className='mx-5 my-24 flex flex-col items-center'>
         <img src={me} height={600}/>
+        <img className='arrow' src={arrow} height={75} width={75} onClick={handleNavigation}/>
       </div>
 
-
-        <div className="vertical-line"></div>
-        <div className='vertical my-auto'>
+        <div className="vertical-line" style={{ backgroundColor: getTabColor() }}></div>
+        <div className='vertical my-auto' style={{ color: getTabColor() }}>
         {activeTab.split('').map((letter, index) => (
           <span key={index} className='vertical-letter'>
             {letter === " " ? '\u00A0' : letter}
           </span>
         ))}
         </div>
-        <div className="vertical-line"></div>
+        <div className="vertical-line" style={{ backgroundColor: getTabColor() }}></div>
 
 
       <div className='mx-3'>
@@ -78,14 +97,14 @@ function Hobbies() {
           fill
           onSelect={(selectedTabs) => setActiveTab(selectedTabs)}
         >
-          <Tab eventKey="MY PASSION" title="MY PASSION" tabClassName='tab-title'>
+          <Tab eventKey="MY PASSION" title="MY PASSION" tabClassName='tab-title' >
             <Passion />
           </Tab>
           <Tab eventKey="ABOUT ME" title="ABOUT ME" tabClassName='tab-title'>
             <About />
           </Tab>
           <Tab eventKey="MY SKILLS" title="SKILLS" tabClassName='tab-title'>
-            <Education />
+            <Skills />
           </Tab>
           <Tab eventKey="MY HOBBIES" title="HOBBIES" tabClassName='tab-title'>
             <Hobbies />
